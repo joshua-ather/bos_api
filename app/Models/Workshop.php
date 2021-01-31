1f715d4636ba700d0ea6020d2237b693f1d3950a
Joshua Ather <joshuaather5@gmail.com>
2021-01-31 17:58:22 +0700

    Create Workshop Model

---
+++ app/Models/Workshop.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Workshop extends Model
+{
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+      'name', 'address'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+      'workshop_id'
+    ];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'workshop';
+
+    protected $primaryKey = 'workshop_id';
+
+}
