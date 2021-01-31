a5a91bc4a25392f02002e08664622817cc7e8569
Joshua Ather <joshuaather5@gmail.com>
2021-01-31 18:33:38 +0700

    Create InventoryItemController

---
+++ app/Http/Controllers/InventoryItemController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class InventoryItemController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+}
