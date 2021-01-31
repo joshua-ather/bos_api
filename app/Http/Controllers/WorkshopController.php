4934181e905d3002f926f5cf013eff66d4ea68a6
Joshua Ather <joshuaather5@gmail.com>
2021-01-31 18:32:23 +0700

    Create WorkshopController

---
+++ app/Http/Controllers/WorkshopController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class WorkshopController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+}
