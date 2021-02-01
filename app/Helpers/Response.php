2a6db11fa63d3b26bcdecce4f092e7db94bbf1d2
Joshua Ather <joshuaather5@gmail.com>
2021-02-01 16:54:38 +0700

    Update to add responseJSON as global function

--- app/Helpers/Response.php
+++ app/Helpers/Response.php
@@ -1,1 +1,20 @@
 <?php
+
+// To get response message as global functioon
+function responseJSON($code, $data, $message)
+{
+    ($code === 3) ? $request = 'success' :
+      (($code === 2) ? $request = 'warning' :
+        $request = 'error');
+
+    $response = [
+      'data'   => $data,
+      'status' => [
+        'code'    => $code,
+        'request' => $request,
+        'message' => $message,
+      ],
+    ];
+
+    return $response;
+}
